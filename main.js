3d60a643bd1b2c37074794c803ea443cc1549330
gabrielbortoluzzi2022 <107260299+gabrielbortoluzzi2022@users.noreply.github.com>
2024-04-03 10:35:24 -0300

    03/04/24
    trabalho

--- main.js
+++ main.js
@@ -9,6 +9,25 @@ botoes[i].onclick = function(){
 
     botoes[i].classList.add("ativo");
 }
-    
+    const botoes = document.querySelectoraALL(".botao");
+    const textos = document. querySelectorALL(".aba-conteudo");
+
+    for(let i=0;i<botoes.length;i++){
+        botoes[i].onclick = function(){
+
+            for(let j=0;j<botoes.length;l++){
+                botoes[j].classList.remove("ativo");
+                textos[j].classList.remove("ativo");
+            }
+        }  
+
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
 
 }
+const contadores = document. querySelectorALL(".contador");
+const tempoObejetivo1 = new Date("2024-10-05T00:00:00")
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObejetivo1 - tempoAtual;
